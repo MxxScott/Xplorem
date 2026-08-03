db097690abde3b1e7656668d3119131000a00e85
David Lawal <dlawal979@gmail.com>
2026-08-03 13:02:10 +0100

    Add routing and scaffold pages/layouts
    Introduce React Router setup and scaffold app structure: update App.jsx to create a browser router with routes for Home, Login, Signup, MediaDetails, Search, Watchlist and a NotFound route. Add RootLayout (with Outlet and Navbar) and stub components/pages: Navbar, Footer, Home, Login, Signup, MediaDetails, Search, Watchlist, NotFound. All new files are placeholders to be implemented with actual UI and logic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import RootLayout from "./Layouts/RootLayout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import MediaDetails from "./pages/MediaDetails";
+import Search from "./pages/Search";
+import Watchlist from "./pages/Watchlist";
+import NotFound from "./pages/NotFound";
+import Signup from "./pages/Signup";
 
 function App() {
-  return <div>
-    
-  </div>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/media/:id" element={<MediaDetails />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/watchlist" element={<Watchlist />} />
+        <Route element={<NotFound />} />
+      </Route>,
+    ),
+  );
+  return <RouterProvider router={router} />;
 }
   
 export default App;
---
+++ src/pages/MediaDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MediaDetails() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MediaDetails
